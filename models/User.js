acf2fc39cfe774c4049be3f33a3da7ed5742c0ac
Ishaan Pota <ishaanpota@s-169-232-241-24.resnet.ucla.edu>
2018-11-17 17:29:28 -0800

    keystone set up

---
+++ models/User.js
@@ -0,0 +1,14 @@
+var keystone = require('keystone');
+
+var User = new keystone.List('User');
+
+User.add({
+    displayName: { type: String },
+    password: { type: keystone.Field.Types.Password },
+    email: { type: keystone.Field.Types.Email, unique: true },
+});
+User.schema.virtual('canAccessKeystone').get(function () {
+    return true;
+});
+User.defaultColumns = 'id, displayName, email';
+User.register();
