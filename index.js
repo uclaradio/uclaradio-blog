256ef40c927ff1f78863a0716ef2a3f6797ee3e0
tanzeelak <tnkhan8042@gmail.com>
2019-02-26 11:40:54 -0800

    plz

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var keystone = require("keystone");
 
 if (keystone.get("env") == "production") {
-  console.log("wassup");
+  console.log("PRODUCTION");
+  // may need to update cloudinary
   keystone.init({
     "cookie secret": "secure string goes here",
     name: "uclaradio-blog",
@@ -9,11 +10,11 @@ if (keystone.get("env") == "production") {
     "auto update": true,
     auth: true,
     static: ["./server/public/js/", "./server/public/img/"],
-    // mongo: process.env.MONGODB_URI,
+    mongo: process.env.MONGODB_URI,
     "cloudinary config": process.env.CLOUDINARY_URL
   });
-  keystone.set("port", process.env.PORT);
 } else {
+  console.log("DEV");
   var config = require("./config.json");
   keystone.init({
     "cookie secret": "secure string goes here",
@@ -27,11 +28,10 @@ if (keystone.get("env") == "production") {
       config.CLOUD_API_SECRET
     }@${config.CLOUD_NAME}`
   });
-  keystone.set("port", 3010);
 }
 
 keystone.import("./server/models");
-keystone.set("port", 3010);
+keystone.set("port", process.env.PORT || 3010);
 keystone.set("routes", require("./server/routes"));
 
 keystone.start();
