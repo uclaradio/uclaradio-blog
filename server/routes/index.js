5fcf9061935d390d7017fb9481506990a03d1dbf
tanzeelak <tnkhan8042@gmail.com>
2019-03-04 22:00:32 -0800

    redirect from index

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,24 +12,6 @@ exports = module.exports = function(app) {
   app.get("/api/posts/:id", keystone.middleware.api, routes.api.posts.get);
   app.get("/api/posts/", keystone.middleware.api, routes.api.posts.list);
   app.get("/", function(req, res) {
-    // Render some simple boilerplate html
-    function renderFullPage() {
-      // Note the div class name here, we will use that as a hook for our React code
-      return `
-		<!doctype html>
-		<html>
-			<head>
-				<title>Keystone With React And Redux</title>
-			</head>
-      <body>
-        <div class="react-container">
-        </div>
-				<script src="bundle.js"></script>
-			</body>
-		</html>
-		`;
-    }
-    // Send the html boilerplate
-    res.send(renderFullPage());
+    res.redirect("/keystone");
   });
 };
